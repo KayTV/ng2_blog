e466236222efbbd26116bbe2033d5b52e1a13ec2
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-12-06 20:56:34 -0500

    added css to blog posts and hover quality

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule }     from './app.router';
 
@@ -20,6 +20,7 @@ import { PostService } from './services/post.service';
     AppRoutingModule,
     HttpModule,
     FormsModule,
+    ReactiveFormsModule,
     InMemoryWebApiModule.forRoot(InMemoryStoreService)
   ],
   declarations: [
