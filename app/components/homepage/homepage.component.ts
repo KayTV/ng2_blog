ba1db13ecee0cab0aabe11b1315dbed035bc9e12
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-12-06 20:04:06 -0500

    added 8 bit font for the site

--- app/components/homepage/homepage.component.ts
+++ app/components/homepage/homepage.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'homepage',
-  templateUrl: 'app/components/homepage/homepage.html'
+  templateUrl: 'app/components/homepage/homepage.html',
+  styleUrls: ['app/styles/homepage.css']
 })
 
 export class HomepageComponent { }
--- app/components/navbar/navbar.component.ts
+++ app/components/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'navbar',
   templateUrl: 'app/components/navbar/navbar.html',
-  styleUrls: ['app/styles/navbar.css']
+  styleUrls: ['app/styles/navbar.css', 'app/styles/shared.css']
 })
 
 export class NavbarComponent { }
